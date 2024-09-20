5480eb2aba4d874e62740a4d729952eafc073651
PariAg06 <124270057+PariAg06@users.noreply.github.com>
2024-09-21 01:42:42 +0530

    Commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+// import React from 'react'
+// import Age_cal from './components/Age_cal'
+// import './App.css'
+// const App = () => {
+//   return (
+//     <div className='App'>
+//  <Age_cal/>
+
+//     </div>
+//   )
+// }
+
+// export default App
+
+import './App.css';
+import Age_cal from './components/Age_cal';
+
+ 
+function App() {
+  return (
+    <div className="App">
+<Age_cal/>
+    </div>
+  );
+}
+ 
+export default App;
